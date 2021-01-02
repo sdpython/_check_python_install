4ec3d726470c1e7c320dc0817270eb9fba4d2d9a
xavier dupré <xavier.dupre@gmail.com>
2021-01-02 19:11:38 +0100

    update CI

--- _unittests/ut_checks/test_cartopy.py
+++ _unittests/ut_checks/test_cartopy.py
@@ -3,12 +3,13 @@
 """
 
 import unittest
-from pyquickhelper.pycode import ExtTestCase
+from pyquickhelper.pycode import ExtTestCase, skipif_azure_linux
 from check_python_install.check_cartopy import check_cartopy
 
 
 class TestCartopy(ExtTestCase):
 
+    @skipif_azure_linux('Crashes on linux with azuredevops')
     def test_cartopy(self):
         ax = check_cartopy()
         self.assertNotEmpty(ax)
